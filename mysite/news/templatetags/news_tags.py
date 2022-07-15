3a7194de67048441fc3b0697ce87bf5ba880c367
marisedas <marisedas@inbox.ru>
2022-07-15 11:42:55 +0300

    добавлено количество записей в категории

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -1,6 +1,7 @@
 from django import template
+from django.db.models import Count
 
-from news.models import Category
+from ..models import Category
 
 register = template.Library()
 
@@ -12,5 +13,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='hello', arg2='world'):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('get_news')).filter(cnt__gt=0)
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
